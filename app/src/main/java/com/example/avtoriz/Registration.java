4c5a2db13400d50f9f67d639cf9b81782fbdcae0
Timur <tfattahov4@gmail.com>
2022-11-17 15:23:52 +0300

    Дополненная авторизация

--- app/src/main/java/com/example/avtoriz/Registration.java
+++ app/src/main/java/com/example/avtoriz/Registration.java
@@ -2,7 +2,9 @@ package com.example.avtoriz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registration extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void registr(View view) {
+        Intent intent;
+        intent = new Intent(Registration.this, Internet.class);
+        startActivity(intent);
     }
 }
